dc47d4b28b0e70171da5af2e0e2c96d3f5547aa1
Laszlo Juracz <laszlo.juracz@vanderbilt.edu>
2014-12-01 15:45:07 -0600

    snapshot

--- src/library/directives/projectBrowser/projectBrowser.js
+++ src/library/directives/projectBrowser/projectBrowser.js
@@ -1,162 +1,175 @@
 /*globals angular, chance*/
 'use strict';
 
-require( '../termFilter/termFilter.js' );
+require('../termFilter/termFilter.js');
 
-angular.module( 'gme.directives.projectBrowser', [
+angular.module('gme.directives.projectBrowser', [
   'gme.templates',
   'isis.ui.itemList',
   'gme.directives.termFilter',
-  'ngTagsInput'
-] )
-  .run( function () {
+  'ngTagsInput',
+  'gme.testServices'
+])
+.run(function () {
 
-  } )
-  .controller( 'ProjectBrowserController', function ( $scope, $log, $filter ) {
+})
+.controller('ProjectBrowserController', function ($scope, $log, $filter, projectServiceTest) {
 
-    var config,
-      dummyProjectGenerator,
-      filterItems,
-      projectList,
-      availableTerms,
-      i;
+  var config,
+  dummyProjectGenerator,
+  filterItems,
+  projectList,
+  availableTerms,
+  i;
 
-    availableTerms = $scope.availableTerms = [ {
+  availableTerms = $scope.availableTerms = [
+    {
       id: 'tag1',
       name: 'Tag A',
       url: 'http://vanderbilt.edu'
-    }, {
+    },
+    {
       id: 'tag2',
       name: 'Tag B',
       url: 'http://vanderbilt.edu'
-    }, {
+    },
+    {
       id: 'tag3',
       name: 'Tag C',
       url: 'http://vanderbilt.edu'
-    }, {
+    },
+    {
       id: 'tag4',
       name: 'Tag D',
       url: 'http://vanderbilt.edu'
-    }, {
+    },
+    {
       id: 'tag5',
       name: 'Tag E',
       url: 'http://vanderbilt.edu'
-    }, {
+    },
+    {
       id: 'tag6',
       name: 'Tag F',
       url: 'http://vanderbilt.edu'
-    } ];
+    }
+  ];
 
-    $scope.filtering = {
-      selectedTermIds: [
+  $scope.filtering = {
+    selectedTermIds: [
 
-      ]
-    };
+    ]
+  };
 
-    projectList = $scope.projectList = {
-      items: []
-    };
+  projectList = $scope.projectList = {
+    items: []
+  };
 
-    $scope.filteredProjectList = {
-      items: []
-    };
+  $scope.filteredProjectList = {
+    items: []
+  };
 
 
-    filterItems = function () {
-      $scope.filteredProjectList.items = $filter( 'termFilter' )( $scope.projectList.items,
-        $scope.filtering.selectedTermIds );
-    };
+  filterItems = function () {
+    $scope.filteredProjectList.items = $filter('termFilter')($scope.projectList.items,
+    $scope.filtering.selectedTermIds);
+  };
 
-    $scope.$watch( function () {
+  $scope.$watch(function () {
 
-        return $scope.filtering.selectedTermIds;
-      }, function () {
-        filterItems();
+    return $scope.filtering.selectedTermIds;
+  }, function () {
+    filterItems();
+  },
+  true);
+
+
+  $scope.$watch('filtering.selectedTermIds', function () {
+    filterItems();
+  });
+
+  $scope.$watch('projectList.items', function () {
+    filterItems();
+  });
+
+  dummyProjectGenerator = function (id) {
+
+    var projectDescriptor, i;
+
+    projectDescriptor = {
+      id: id,
+      title: chance.paragraph({
+        sentences: 1
+      }),
+      cssClass: 'project-item',
+      toolTip: 'Open project',
+      description: chance.paragraph({
+        sentences: 2
+      }),
+      lastUpdated: {
+        time: Date.now(),
+        user: 'N/A'
+
       },
-      true );
-
-
-    $scope.$watch( 'filtering.selectedTermIds', function () {
-      filterItems();
-    } );
-
-    $scope.$watch( 'projectList.items', function () {
-      filterItems();
-    } );
-
-    dummyProjectGenerator = function ( id ) {
-
-      var projectDescriptor, i;
-
-      projectDescriptor = {
-        id: id,
-        title: chance.paragraph( {
-          sentences: 1
-        } ),
-        cssClass: 'project-item',
-        toolTip: 'Open project',
-        description: chance.paragraph( {
-          sentences: 2
-        } ),
-        lastUpdated: {
-          time: Date.now(),
-          user: 'N/A'
-
-        },
-        taxonomyTerms: [],
-        stats: [ {
+      taxonomyTerms: [],
+      stats: [
+        {
           value: id,
           toolTip: 'Commits',
           iconClass: 'fa fa-cloud-upload'
-        }, {
+        },
+        {
           value: id,
           toolTip: 'Users',
           iconClass: 'fa fa-users'
-        } ],
-        details: chance.paragraph( {
-          sentences: 3
-        } )
-      };
-
-      for ( i = 0; i < $scope.availableTerms.length - 1; i++ ) {
-
-        if ( Math.random() > 0.5 ) {
-          projectDescriptor.taxonomyTerms.push( $scope.availableTerms[ i ] );
         }
-      }
-
-      return projectDescriptor;
-
+      ],
+      details: chance.paragraph({
+        sentences: 3
+      })
     };
 
+    for (i = 0; i < $scope.availableTerms.length - 1; i++) {
 
-    for ( i = 0; i < 20; i++ ) {
-      $scope.projectList.items.push( dummyProjectGenerator( i ) );
+      if (Math.random() > 0.5) {
+        projectDescriptor.taxonomyTerms.push($scope.availableTerms[ i ]);
+      }
     }
 
-    $scope.config = config = {
+    return projectDescriptor;
 
-      sortable: true,
-      secondaryItemMenu: true,
-      detailsCollapsible: true,
-      showDetailsLabel: 'Show details',
-      hideDetailsLabel: 'Hide details',
-      filter: {},
+  };
 
-      // Event handlers
+//  for (i = 0; i < 20; i++) {
+//    $scope.projectList.items.push(dummyProjectGenerator(i));
+//  }
 
-      itemSort: function ( jQEvent, ui ) {
-        console.log( 'Sort happened', jQEvent, ui );
-      },
 
-      itemClick: function ( event, item ) {
-        console.log( 'Clicked: ' + item );
-      },
 
-      itemContextmenuRenderer: function ( e, item ) {
-        console.log( 'Contextmenu was triggered for node:', item );
+  $scope.config = config = {
 
-        return [ {
+    sortable: true,
+    secondaryItemMenu: true,
+    detailsCollapsible: true,
+    showDetailsLabel: 'Show details',
+    hideDetailsLabel: 'Hide details',
+    filter: {},
+
+    // Event handlers
+
+    itemSort: function (jQEvent, ui) {
+      console.log('Sort happened', jQEvent, ui);
+    },
+
+    itemClick: function (event, item) {
+      console.log('Clicked: ' + item);
+    },
+
+    itemContextmenuRenderer: function (e, item) {
+      console.log('Contextmenu was triggered for node:', item);
+
+      return [
+        {
           items: [
 
             {
@@ -166,49 +179,50 @@ angular.module( 'gme.directives.projectBrowser', [
               iconClass: 'fa fa-plus'
             }
           ]
-        } ];
-      },
+        }
+      ];
+    },
 
-      detailsRenderer: function ( item ) {
-        item.details = 'My details are here now!';
-      },
+    detailsRenderer: function (item) {
+      item.details = 'My details are here now!';
+    },
 
-      newItemForm: {
-        title: 'Create new Project',
-        itemTemplateUrl: '/ng-gme/templates/newProjectTemplate.html',
-        expanded: false,
-        controller: function ( $scope ) {
+    newItemForm: {
+      title: 'Create new Project',
+      itemTemplateUrl: '/ng-gme/templates/newProjectTemplate.html',
+      expanded: false,
+      controller: function ($scope) {
+
+        $scope.newItem = {};
+
+        $scope.tags = angular.copy(availableTerms);
+
+        $scope.createItem = function (newItem) {
+
+          newItem.id = newItem.title;
+          projectList.items.push(newItem);
+          console.log(projectList.items);
 
           $scope.newItem = {};
 
-          $scope.tags = angular.copy( availableTerms );
+          config.newItemForm.expanded = false; // this is how you close the form itself
 
-          $scope.createItem = function ( newItem ) {
+        };
 
-            newItem.id = newItem.title;
-            projectList.items.push( newItem );
-            console.log( projectList.items );
-
-            $scope.newItem = {};
-
-            config.newItemForm.expanded = false; // this is how you close the form itself
-
-          };
-
-        }
       }
+    }
 
-    };
+  };
 
 
-  } )
-  .directive( 'projectBrowser', function () {
+})
+.directive('projectBrowser', function () {
 
-    return {
-      scope: false,
-      restrict: 'E',
-      controller: 'ProjectBrowserController',
-      replace: true,
-      templateUrl: '/ng-gme/templates/projectBrowser.html'
-    };
-  } );
+  return {
+    scope: false,
+    restrict: 'E',
+    controller: 'ProjectBrowserController',
+    replace: true,
+    templateUrl: '/ng-gme/templates/projectBrowser.html'
+  };
+});
