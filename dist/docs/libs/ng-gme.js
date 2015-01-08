b4d327d91506c02153afc8b22488d592cefc0ad6
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-01-07 18:16:25 -0600

    BranchService: add createBranch

--- dist/docs/libs/ng-gme.js
+++ dist/docs/libs/ng-gme.js
@@ -526,6 +526,25 @@ module.exports = function ( $q, dataStoreService, projectService ) {
         return deferred.promise;
     };
 
+    this.createBranch = function ( databaseId, branchId, hash ) {
+        var dbConn = dataStoreService.getDatabaseConnection( databaseId ),
+            deferred = new $q.defer();
+
+        dbConn.branchService = dbConn.branchService || {};
+
+        dbConn.client.createBranchAsync( branchId, hash,
+            function ( err ) {
+                if ( err ) {
+                    deferred.reject( err );
+                    return;
+                }
+
+                deferred.resolve( branchId );
+            } );
+
+        return deferred.promise;
+    };
+
     this.getSelectedBranch = function ( /*databaseId*/) {
         throw new Error( 'Not implemented yet.' );
     };
